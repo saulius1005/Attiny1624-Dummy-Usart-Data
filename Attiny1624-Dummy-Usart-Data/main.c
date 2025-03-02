a68d7e5c3eb33ea0a47dd045c1e68247c9b6212d
saulius1005 <saulius1005@gmail.com>
2025-03-02 21:47:38 +0200

    Automatic CRC calculations
    Added a function that will automatically handle all variables and combine them into one uint64_t variable. This will make CRC calculation easier.

--- Attiny1624-Dummy-Usart-Data/main.c
+++ Attiny1624-Dummy-Usart-Data/main.c
@@ -5,6 +5,13 @@
  * Author : Saulius
  */ 
 #include "Settings.h"
+		uint16_t a = 90, //elevacijos kampas apie 90 laipsniø +-
+				 b = 35999, //azimuto kampas max 35999
+				 c = 2515; //átampa max teorinë apie 250V +-
+		uint8_t  d = 101, //max srove apie 10A +-
+				 e = 1; //Y endswitch ámitavimui. 0,1,2,3
+
+		uint64_t combined = 0;
 
 int main(void)
 {
@@ -12,11 +19,13 @@ int main(void)
     GPIO_init(); ///< Initialize GPIO pins
     USART0_init(); ///< Initialize USART0 for SPI communication
 
+	combined = ((uint64_t)a << 40) | ((uint64_t)b << 24) | ((uint32_t)c << 12) | ((uint16_t)d << 4) | e;
+
     while (1) 
     {
 		//aaaa- elevacijos kampas, bbbb-azimuto kampas, ccc sp-átampa, dd sp-srovë, e y min + y max (0-neivienas, 1-min arba 2-max)
-       // USART0_sendString("<aaaa|bbbb|ccc|dd|e>\r\n"); //~25 simboliai
-		USART0_printf("<%04x%04x%03x%02x%x%02x>\r\n",(uint16_t)8999,(uint16_t)35999, (uint16_t)2591, (uint8_t)105, (uint8_t)0, (uint8_t)crc8_cdma2000(0x23278C9FA1F690)); //imituojam 89.99 elevacijos kampa, 359.99 azimuto kampa,spU 259.1V,spI 10.5A
+       // USART0_sendString("<aaaabbbbcccdde>\r\n"); //18 simboliø
+		USART0_printf("<%04x%04x%03x%02x%x%02x>\r\n", (uint16_t)a, (uint16_t)b, (uint16_t)c, (uint8_t)d, (uint8_t)e, (uint8_t)crc8_cdma2000(combined)); //imituojam 89.99 elevacijos kampa, 359.99 azimuto kampa,spU 259.1V,spI 10.5A
         _delay_ms(100); ///< Wait 100ms before the next read
     }
 }
